8127e976e2e7e8d7e94c03ea5d0b1eadb2768c83
nichoth <nichoth@gmail.com>
2021-06-06 09:47:27 -0700

    settung up fauna

---
+++ README.md
@@ -0,0 +1,15 @@
+# SSC fauna
+
+SSC db built with [fauna db](https://fauna.com/)
+
+
+---------------------------------------------------
+
+1. Make a DB in fauna and put the secret key in `.env`.
+
+.env
+```
+FAUNADB_SERVER_SECRET="123"
+```
+
+
---
+++ avatar.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+var faunadb = require('faunadb')
+var createHash = require('crypto').createHash
+
+var q = faunadb.query
+var client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET
+})
+
+function get (aboutWho) {
+    return client.query(
+        q.Get( q.Match(q.Index('avatar-by-id'), aboutWho) )
+    )
+}
+
+function post (keys, file) {
+    var hash = getHash(file)
+
+    return client.query(
+        q.If(
+            q.Exists(
+                q.Select('ref', q.Get(
+                    q.Match(q.Index('avatar-by-id'), '@' + keys.public)
+                ))
+            ),
+            q.Replace(
+                q.Select('ref', q.Get(
+                    q.Match(q.Index('avatar-by-id'), '@' + keys.public)
+                )),
+                { data: { about: '@' + keys.public, avatarLink: hash } },
+            ),
+            q.Create(
+                q.Collection('avatar'),
+                { data: { about: '@' + keys.public, avatarLink: hash } },
+            )
+        )
+    )
+}
+
+function getHash (file) {
+    var hash = createHash('sha256')
+    hash.update(file)
+    var _hash = hash.digest('base64')
+    return _hash
+}
+
+module.exports = {
+    get,
+    post
+}
+
