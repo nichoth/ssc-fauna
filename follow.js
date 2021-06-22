1e8d88cce72a74eb615bfb5648367dcb797eb1cf
nichoth <nichoth@gmail.com>
2021-06-22 13:27:46 -0700

    get the avatar .data

--- follow.js
+++ follow.js
@@ -31,11 +31,13 @@ function get (author) {
         })
         .then(([followed, avas, names]) => {
             // console.log('****aaaaa fol, ava, names', followed, avas, names)
-            var fols = followed.reduce((acc, doc, i) => {
+            var fols = followed.reduce((acc, id, i) => {
                 // console.log('**doc**', doc)
-                acc[doc] = {
-                    id: doc,
-                    avatar: (avas && avas[i]) || null,
+                acc[id] = {
+                    id: id,
+                    avatar: (avas && avas[i] && avas[i][0]) ?
+                        avas[i][0].data :
+                        null,
                     name: (names && names[i]) || null
                 }
                 return acc
@@ -82,7 +84,6 @@ function getAvatars (followed) {
     )
         .then(res => {
             var _res = res.map(doc => doc.data)
-            // console.log('ava res', _res)
             return _res
         })
         .catch(err => {
@@ -91,8 +92,6 @@ function getAvatars (followed) {
 }
 
 function getNames (followed) {
-    // console.log('in get names', followed)
-
     return client.query(
         q.Map(
             followed,
@@ -118,55 +117,7 @@ function getNames (followed) {
         })
 }
 
-
-// function get (author) {
-//     return client.query(
-//         q.Map(
-//             q.Paginate(
-//                 q.Reverse( q.Match(q.Index('following'), author) )
-//             ),
-//             q.Lambda( 'followMsg', q.Get(q.Var('followMsg')) )
-//         )
-//     )
-//         .then(res => {
-//             // TODO
-//             // should get the avatar & profile info for each item in the array
-
-//             // return res.data.map(doc => doc.data)
-//             // TODO -- need to check the msg value for `following: false`
-//             var map = res.data.reduce((acc, doc) => {
-//                 // TODO -- should be the person's profile data
-//                 // need to do another query to get the profile data
-//                 acc[doc.data.value.content.contact] = doc.data
-//                 return acc
-//             }, {})
-
-//             return map
-//         })
-// }
-
 async function post (author, keys, msg) {
-    // try {
-    //     var lastFollowMsg = await client.query(
-    //         q.Get(
-    //             q.Match(q.Index('following'), author)
-    //         )
-    //     );
-    // } catch (err) {
-    //     if (err.message === 'instance not found') {
-    //         // this means it's a new string of 'follow' msgs
-    //         // and there is no ancestor
-    //         // console.log('~~~~~ not found ~~~~~')
-    //         var lastFollowMsg = null
-    //     } else {
-    //         throw err
-    //     }
-    // }
-
-    // var lastMsg = lastFollowMsg ? lastFollowMsg.data.value : null
-
-    // console.log('last follow*****', lastMsg)
-
     try {
         var isValid = ssc.verifyObj(keys, null, msg)
     } catch (err) {
