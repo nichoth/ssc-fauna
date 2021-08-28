eb27a578b3389dc76a2608a9a09688d04d05097d
nichoth <nichoth@gmail.com>
2021-08-28 14:38:22 -0700

    refactor unfollow test a little

--- test/unfollow.js
+++ test/unfollow.js
@@ -8,15 +8,69 @@ var follow = require('../follow')
 var keys = ssc.createKeys()
 var userTwo = ssc.createKeys()
 
-test('follow then unfollow someone', function (t) {
-    // create a name for userTwo
-    profile.post(userTwo.id, null, ssc.createMsg(keys, null, {
+function createProfile (user) {
+    return profile.post(user.id, null, ssc.createMsg(user, null, {
         type: 'profile',
-        about: userTwo.id,
+        about: user.id,
         name: 'fooo'
     }))
+}
+
+function followThem (user, followed) {
+    // need to create a msg for post req
+    var msg = ssc.createMsg(user, null, {
+        type: 'follow',
+        contact: followed.id,
+        author: user.id
+    })
+
+    // this should return the profile document for the followed user
+    return follow.post(user, msg)
         .then((res) => {
-            return followThem()
+            return res
+        })
+        .catch(err => {
+            console.log('***** ick', err)
+            t.error(err)
+            t.end()
+        })
+}
+
+function _unfollow (user, contact) {
+    // need to create a msg for post req
+    var unfollowMsg = ssc.createMsg(user, null, {
+        type: 'unfollow',
+        contact: contact.id,
+        author: user.id
+    })
+
+    return unfollow.post(keys, unfollowMsg)
+        .then(res => {
+            return res
+        })
+}
+
+test('follow then unfollow someone', function (t) {
+    // create a name for userTwo
+    createProfile(userTwo)
+        .then(() => followThem(keys, userTwo))
+        .then((res) => {
+            t.pass('should create a follow document')
+            t.equal(res.value.content.name, 'fooo',
+                'should return the user profile of the person that ' +
+                'youre following')
+            t.equal(res.value.content.about, userTwo.id,  
+                'should have the right user ID')
+            return res
+        })
+        .then(res => {
+            return _unfollow(keys, userTwo)
+        })
+        .then(res => {
+            t.pass('unfollowed')
+            t.equal(res.contact, userTwo.id,
+                'should send back the id of who you unfollowed')
+            t.end()
         })
         .catch(err => {
             t.fail('got an error')
@@ -24,55 +78,8 @@ test('follow then unfollow someone', function (t) {
             t.end()
         })
 
-    function followThem () {
-        var msgContent = {
-            type: 'follow',
-            contact: userTwo.id,
-            author: keys.id
-        }
+})
 
-        // need to create a msg for post req
-        msg = ssc.createMsg(keys, null, msgContent)
-
-        // this should return the profile document for the followed user
-        return follow.post(keys, msg)
-            .then((res) => {
-                t.pass('should create a follow document')
-                t.equal(res.value.content.name, 'fooo',
-                    'should return the user profile of the person that ' +
-                    'youre following')
-                t.equal(res.value.content.about, userTwo.id,  
-                    'should have the right user ID')
-
-                return _unfollow()
-            })
-            .catch(err => {
-                console.log('***** ick', err)
-                t.error(err)
-                t.end()
-            })
-    }
-
-    function _unfollow () {
-        // need to create a msg for post req
-        var unfollowMsg = ssc.createMsg(keys, null, {
-            type: 'unfollow',
-            contact: userTwo.id,
-            author: keys.id
-        })
-
-        return unfollow.post(keys, unfollowMsg)
-            .then(res => {
-                console.log('**unfollow response**', res)
-                t.pass('unfollowed')
-                t.equal(res.contact, userTwo.id,
-                    'should send back the id of who you unfollowed')
-                t.end()
-            })
-            .catch(err => {
-                t.fail('got an error')
-                console.log('errrrrr', err)
-                t.end()
-            })
-    }
+test('another user follows, then you unfollow', t => {
+    t.end()
 })
